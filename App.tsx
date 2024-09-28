b42145fa9737e50128bf56de356a95bd99af7078
chawkitariq <chawkitariq@icloud.com>
2024-09-28 14:26:25 +0200

    refactor: clean code

--- App.tsx
+++ App.tsx
@@ -1,23 +1,8 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-function DetailScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Detail Screen</Text>
-    </View>
-  );
-}
+import HomeScreen from './src/screens/home';
+import DetailScreen from './src/screens/detail';
 
 const Stack = createNativeStackNavigator();
 
@@ -26,7 +11,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Detail" component={DetailScreen} />
+          <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/detail.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+function DetailScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Detail Screen</Text>
+    </View>
+  );
+}
+
+export default DetailScreen;
