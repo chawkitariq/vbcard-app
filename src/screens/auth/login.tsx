98fe6684a0fbaa65633990228f2d28a85a955056
chawkitariq <chawkitariq@icloud.com>
2024-09-28 23:25:25 +0200

    feat: setup login form validation

--- src/screens/auth/login.tsx
+++ src/screens/auth/login.tsx
@@ -1,11 +1,19 @@
 import {useMutation} from '@tanstack/react-query';
 import {Formik} from 'formik';
-import {View} from 'react-native';
-import {Button, TextInput} from 'react-native-paper';
+import {Text, View} from 'react-native';
+import {Button, HelperText, TextInput} from 'react-native-paper';
 import {AuthApiService} from '../../services';
 import {useAuthStore} from '../../stores';
 import {AuthLoginResponsePayload} from '../../types';
 import {useState} from 'react';
+import {object, string} from 'yup';
+
+const validationSchema = object().shape({
+  email: string()
+    .email('Doit être une adresse mail valide')
+    .required('Obligatoire'),
+  password: string().required('Obligatoire'),
+});
 
 const LoginScreen = (props: any) => {
   const [isPasswordHidden, setIsPasswordHidden] = useState(true);
@@ -22,8 +30,12 @@ const LoginScreen = (props: any) => {
   });
 
   return (
-    <Formik initialValues={{email: '', password: ''}} onSubmit={handleLogin}>
-      {({handleChange, handleBlur, handleSubmit, values}) => (
+    <Formik
+      initialValues={{email: '', password: ''}}
+      validationSchema={validationSchema}
+      validateOnChange={true}
+      onSubmit={handleLogin}>
+      {({handleChange, handleBlur, handleSubmit, errors, touched, values}) => (
         <View
           style={{
             minHeight: '100%',
@@ -31,28 +43,40 @@ const LoginScreen = (props: any) => {
             padding: 24,
             gap: 16,
           }}>
-          <TextInput
-            mode="outlined"
-            onChangeText={handleChange('email')}
-            onBlur={handleBlur('email')}
-            value={values.email}
-            keyboardType="email-address"
-            placeholder="Adresse mail"
-          />
-          <TextInput
-            mode="outlined"
-            onChangeText={handleChange('password')}
-            onBlur={handleBlur('password')}
-            value={values.password}
-            placeholder="Mot de passe"
-            secureTextEntry={isPasswordHidden}
-            right={
-              <TextInput.Icon
-                icon={isPasswordHidden ? 'eye' : 'eye-off'}
-                onPress={() => setIsPasswordHidden(s => !s)}
-              />
-            }
-          />
+          <View>
+            <TextInput
+              mode="outlined"
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+              keyboardType="email-address"
+              placeholder="Adresse mail"
+              error={!!(errors.email && touched.email)}
+            />
+            {errors.email && touched.email && (
+              <HelperText type="error">{errors.email}</HelperText>
+            )}
+          </View>
+          <View>
+            <TextInput
+              mode="outlined"
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              value={values.password}
+              placeholder="Mot de passe"
+              secureTextEntry={isPasswordHidden}
+              error={!!(errors.password && touched.password)}
+              right={
+                <TextInput.Icon
+                  icon={isPasswordHidden ? 'eye' : 'eye-off'}
+                  onPress={() => setIsPasswordHidden(s => !s)}
+                />
+              }
+            />
+            {errors.password && touched.password && (
+              <HelperText type="error">{errors.password}</HelperText>
+            )}
+          </View>
           <Button
             style={{paddingVertical: 16 / 3, borderRadius: 16 / 3}}
             mode="contained"
