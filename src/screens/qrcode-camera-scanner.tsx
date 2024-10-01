2d029d58a5f3f448a2016e2442a5c5b2e7ebd681
chawkitariq <chawkitariq@icloud.com>
2024-10-01 20:45:15 +0200

    feat: use camera

--- src/screens/qrcode-camera-scanner.tsx
+++ src/screens/qrcode-camera-scanner.tsx
@@ -1,22 +1,54 @@
+import {useIsFocused} from '@react-navigation/native';
+import React, {useEffect, useLayoutEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
-import {IconButton, useTheme} from 'react-native-paper';
+import {Appbar, IconButton, useTheme} from 'react-native-paper';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+  useCodeScanner,
+} from 'react-native-vision-camera';
 
 function QrCodeCameraScannerScreen({navigation}: any) {
   const theme = useTheme();
 
+  const isFocused = useIsFocused();
+
+  const device = useCameraDevice('back');
+  const {hasPermission} = useCameraPermission();
+
+  useLayoutEffect(() => {
+    if (!hasPermission) {
+      navigation.goBack();
+    }
+
+    if (device == null) {
+      navigation.goBack();
+    }
+  }, [hasPermission, device, navigation]);
+
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr'],
+    onCodeScanned: codes => {
+      console.log(`Scanned ${codes.length} codes!`);
+    },
+  });
+
   return (
-    <View
-      style={[
-        {backgroundColor: theme.colors.backdrop},
-        StyleSheet.absoluteFill,
-      ]}>
+    <>
       <IconButton
-        style={{position: 'relative', zIndex: 99}}
-        mode="contained"
+        style={{position: 'relative', zIndex: 999}}
         icon="close"
         onPress={() => navigation.goBack()}
+        iconColor={theme.colors.surface}
       />
-    </View>
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device!}
+        isActive={isFocused}
+        codeScanner={codeScanner}
+      />
+    </>
   );
 }
 
